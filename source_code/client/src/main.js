4722ea9ac93f8475758691c81b9585639e6b165b
Lnola <nola.luka@gmail.com>
2022-10-26 17:15:13 +0200

    Add autofocus directive to the entire app

--- source_code/client/src/main.js
+++ source_code/client/src/main.js
@@ -2,5 +2,6 @@ import App from './App.vue';
 import { createApp } from 'vue';
 import naive from 'naive-ui';
 import router from './router';
+import { vAutofocus } from './directives';
 
-createApp(App).use(router).use(naive).mount('#app');
+createApp(App).use(router).use(naive).directive('autofocus', vAutofocus).mount('#app');
