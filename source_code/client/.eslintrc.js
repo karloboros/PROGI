7b91967e0e14b068ef19818cd11a681ead62fcbb
Lnola <nola.luka@gmail.com>
2022-10-26 14:21:29 +0200

    Add no relative import paths rule

--- source_code/client/.eslintrc.js
+++ source_code/client/.eslintrc.js
@@ -2,4 +2,6 @@ module.exports = {
   root: true,
   extends: ['@extensionengine', 'plugin:prettier/recommended'],
   parserOptions: { sourceType: 'module' },
+  plugins: ['no-relative-import-paths'],
+  rules: { 'no-relative-import-paths/no-relative-import-paths': ['warn', { allowSameFolder: true }] },
 };
