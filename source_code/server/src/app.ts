57b273dfd6b4b0c222afbe49737d91413a87b45e
Lnola <nola.luka@gmail.com>
2022-11-10 20:42:22 +0100

    Register router to app

--- source_code/server/src/app.ts
+++ source_code/server/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import router from 'router';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use('/api', router);
 
 app.listen(port, () => console.log(`App is listening on port ${port} !`));
