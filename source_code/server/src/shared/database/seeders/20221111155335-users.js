6863e07b151697371da27bda98aee7da7702df41
Lnola <nola.luka@gmail.com>
2022-11-11 23:55:58 +0100

    Update seeded phone to be unique

--- source_code/server/src/shared/database/seeders/20221111155335-users.js
+++ source_code/server/src/shared/database/seeders/20221111155335-users.js
@@ -12,7 +12,7 @@ const createUser = (index, role = 3) => {
     password,
     gender: 0,
     dateOfBirth: new Date('2001'),
-    phone: '0987654321',
+    phone: `098765432${index}`,
     email: `example${index}@gmail.com`,
     role,
     experienceDescription: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
