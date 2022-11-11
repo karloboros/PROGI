253b3bff915ea73e1e54bfcf61c87f6cee766023
Lnola <nola.luka@gmail.com>
2022-11-12 00:23:02 +0100

    Fix logout path typo

--- source_code/server/src/user/index.ts
+++ source_code/server/src/user/index.ts
@@ -4,6 +4,6 @@ import { Router } from 'express';
 const router = Router();
 const path = '/users';
 
-router.post('/login', login).post('/register', register).post('logout', logout);
+router.post('/login', login).post('/register', register).post('/logout', logout);
 
 export default { router, path };
