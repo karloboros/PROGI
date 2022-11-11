1fb869fcf3a3529f89a46b89feccb6589c60783b
Lnola <nola.luka@gmail.com>
2022-11-11 16:45:34 +0100

    Add create user migration

---
+++ source_code/server/src/shared/database/migrations/20221111153545-create-user.js
@@ -0,0 +1,63 @@
+const TABLE_NAME = 'users';
+
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable(TABLE_NAME, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      gender: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      dateOfBirth: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      experienceDescription: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      refreshToken: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+    });
+  },
+
+  down(queryInterface) {
+    return queryInterface.dropTable(TABLE_NAME);
+  },
+};
