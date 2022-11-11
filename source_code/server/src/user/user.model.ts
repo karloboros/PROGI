8f9e81ce7c19d810d6c6b724430d13746b0520e5
Lnola <nola.luka@gmail.com>
2022-11-11 16:34:19 +0100

    Create user model

---
+++ source_code/server/src/user/user.model.ts
@@ -0,0 +1,107 @@
+import { Gender, IUser, Role } from './types';
+import { IFields } from 'shared/database/types';
+import { Model } from 'sequelize';
+
+class User extends Model implements IUser {
+  id!: number;
+  username!: string;
+  firstName!: string;
+  lastName!: string;
+  fullName!: string;
+  password!: string;
+  gender!: Gender;
+  dateOfBirth!: Date;
+  phone!: string;
+  email!: string;
+  role!: Role;
+  experienceDescription?: string | undefined;
+  refreshToken!: string;
+
+  static fields({ INTEGER, STRING, DATE, VIRTUAL }: IFields) {
+    return {
+      id: {
+        type: INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: STRING,
+        allowNull: false,
+        unique: true,
+      },
+      firstName: {
+        type: STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: STRING,
+        allowNull: false,
+      },
+      fullName: {
+        type: VIRTUAL,
+        get(this: IUser) {
+          return `${this.firstName} ${this.lastName}`.trim();
+        },
+      },
+      password: {
+        type: STRING,
+        allowNull: false,
+      },
+      gender: {
+        type: INTEGER,
+        allowNull: false,
+      },
+      dateOfBirth: {
+        type: DATE,
+        allowNull: false,
+      },
+      phone: {
+        type: STRING,
+        allowNull: false,
+      },
+      email: {
+        type: STRING,
+        allowNull: false,
+      },
+      role: {
+        type: INTEGER,
+        allowNull: false,
+      },
+      experienceDescription: {
+        type: STRING,
+        allowNull: true,
+      },
+      refreshToken: {
+        type: STRING,
+        allowNull: true,
+      },
+    };
+  }
+
+  get profile() {
+    return {
+      id: this.id,
+      username: this.username,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      fullName: this.fullName,
+      password: this.password,
+      gender: this.gender,
+      dateOfBirth: this.dateOfBirth,
+      phone: this.phone,
+      email: this.email,
+      role: this.role,
+      experienceDescription: this.experienceDescription,
+    };
+  }
+
+  static dbOptions() {
+    return {
+      modelName: 'user',
+      tableName: 'users',
+      timestamps: false,
+    };
+  }
+}
+
+export default User;
