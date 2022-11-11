1c144132d8f3a03471b4e529aa43df47dfbd6a01
Lnola <nola.luka@gmail.com>
2022-11-12 00:29:23 +0100

    Create an authenticate method

---
+++ source_code/server/src/shared/auth/authenticate.ts
@@ -0,0 +1,38 @@
+import { JsonWebTokenError, verify as jwtVerify, TokenExpiredError } from 'jsonwebtoken';
+import { NextFunction, Request, Response } from 'express';
+import Audience from './audience';
+import errorMessages from 'shared/constants/errorMessages';
+import { FORBIDDEN } from 'http-status';
+import HttpError from 'shared/error/httpError';
+import { User } from 'shared/database';
+
+const authenticate = async (req: Request, _res: Response, next: NextFunction) => {
+  const { accessToken } = req.cookies;
+  if (!accessToken) {
+    return next(new HttpError(FORBIDDEN, errorMessages.FORBIDDEN));
+  }
+
+  try {
+    const { id, aud } = jwtVerify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+    if (aud !== Audience.Scope.Access) {
+      return next(new HttpError(FORBIDDEN, errorMessages.FORBIDDEN));
+    }
+
+    const user = await User.findByPk(id);
+    if (!user) {
+      return next(new HttpError(FORBIDDEN, errorMessages.FORBIDDEN));
+    }
+
+    req.user = user;
+
+    return next();
+  } catch (err) {
+    if (err instanceof TokenExpiredError) return next();
+
+    if (err instanceof JsonWebTokenError) {
+      return next(new HttpError(FORBIDDEN, errorMessages.FORBIDDEN));
+    }
+  }
+};
+
+export default authenticate;
