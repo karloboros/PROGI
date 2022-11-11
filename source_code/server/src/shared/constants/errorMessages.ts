6aaa0e5638e9a764f45b4e27c7c4aae2ebdca1e3
Lnola <nola.luka@gmail.com>
2022-11-11 23:52:29 +0100

    Add common error messages

---
+++ source_code/server/src/shared/constants/errorMessages.ts
@@ -0,0 +1,8 @@
+const errorMessages = {
+  SIGN_IN: 'Incorrect user credentials.',
+  REGISTER: 'Unable to create user, try different credentials.',
+  FORBIDDEN: 'Your access to this resource is forbidden.',
+  TOKEN_EXPIRED: 'Token has expired',
+};
+
+export default errorMessages;
