806bd340d94379efac8847ce3ab410920d8c79a9
Lnola <nola.luka@gmail.com>
2022-11-12 03:38:46 +0100

    Create the register form

---
+++ source_code/client/src/components/auth/UserRegister.vue
@@ -0,0 +1,102 @@
+<template>
+  <n-form @submit.prevent="submit" :model="values" :rules="rules">
+    <n-form-item label="Username" path="username">
+      <n-input v-model:value="values.username" placeholder="Username..." autofocus />
+    </n-form-item>
+    <n-form-item label="First name" path="firstName">
+      <n-input v-model:value="values.firstName" placeholder="First name..." />
+    </n-form-item>
+    <n-form-item label="Last name" path="lastName">
+      <n-input v-model:value="values.lastName" placeholder="Last name..." />
+    </n-form-item>
+    <n-form-item label="Email" path="email">
+      <n-auto-complete
+        v-model:value="values.email"
+        :input-props="{ autocomplete: 'disabled' }"
+        :options="emailOptions"
+        placeholder="Email..."
+      />
+    </n-form-item>
+    <n-form-item label="Password" path="password">
+      <n-input v-model:value="values.password" type="password" show-password-on="mousedown" placeholder="Password..." />
+    </n-form-item>
+    <n-form-item label="Gender" path="gender">
+      <n-space>
+        <n-radio
+          @change="values.gender = gender.male"
+          :checked="values.gender === gender.male"
+          :value="gender.male"
+          name="gender"
+        >
+          Male
+        </n-radio>
+        <n-radio
+          @change="values.gender = gender.female"
+          :checked="values.gender === gender.female"
+          :value="gender.female"
+          name="gender"
+        >
+          Female
+        </n-radio>
+      </n-space>
+    </n-form-item>
+    <n-form-item label="Date of birth" path="dateOfBirth">
+      <n-date-picker v-model:value="values.dateOfBirth" type="date" />
+    </n-form-item>
+    <n-form-item label="Phone number" path="phone">
+      <n-input v-model:value="values.phone" placeholder="e.g. 098 234 5678..." />
+    </n-form-item>
+    <n-form-item label="Dance experience" path="experienceDescription">
+      <n-input v-model:value="values.experienceDescription" type="textarea" placeholder="Dance experience..." />
+    </n-form-item>
+
+    <n-form-item>
+      <n-button attr-type="submit">Register</n-button>
+    </n-form-item>
+    <n-form-item>
+      <n-p>
+        Already have an account?
+        <n-a @click="$emit('swap')" quaternary>Click here to log in!</n-a>
+      </n-p>
+    </n-form-item>
+  </n-form>
+</template>
+
+<script setup>
+import { computed } from '@vue/reactivity';
+import emailSuggestions from '@/shared/emailSuggestions';
+import gender from '@/shared/gender';
+import { ref } from 'vue';
+import validationRules from '@/shared/rules';
+
+const initialValues = {
+  username: '',
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  gender: gender.male,
+  dateOfBirth: null,
+  phone: '',
+  experienceDescription: '',
+};
+
+const { required, dateRequired, phoneRequired } = validationRules;
+const rules = {
+  username: required,
+  firstName: required,
+  lastName: required,
+  email: required,
+  password: required,
+  dateOfBirth: dateRequired,
+  phone: phoneRequired,
+};
+
+const values = ref(initialValues);
+
+const emailOptions = computed(() => emailSuggestions(values.value.email));
+
+const submit = () => {
+  alert('submit');
+};
+</script>
