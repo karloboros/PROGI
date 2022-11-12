4ec8b12b6e26ca891f7db7f62c83d4a05644589e
Lnola <nola.luka@gmail.com>
2022-11-12 03:38:07 +0100

    Add phone number custom validator

--- source_code/client/src/shared/rules.js
+++ source_code/client/src/shared/rules.js
@@ -1,4 +1,14 @@
 const REQUIRED_MESSAGE = 'This field is required!';
+const WRONG_FORMAT_MESSAGE = 'Wrong number format';
+
+const phoneNumberValidator = (_rule, value) => {
+  if (!value) {
+    return new Error(REQUIRED_MESSAGE);
+  } else if (!/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{3,4})$/.test(value)) {
+    return new Error(WRONG_FORMAT_MESSAGE);
+  }
+  return true;
+};
 
 const validationRules = {
   required: {
@@ -11,6 +21,11 @@ const validationRules = {
     type: 'number',
     message: REQUIRED_MESSAGE,
     trigger: ['blur', 'change'],
+  },
+  phoneRequired: {
+    required: true,
+    validator: phoneNumberValidator,
+    trigger: ['blue', 'input'],
   },
 };
 
