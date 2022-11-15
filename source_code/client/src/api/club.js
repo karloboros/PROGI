69954bb3826368202738584f7c0c7c864b16a276
Lnola <nola.luka@gmail.com>
2022-11-15 21:30:15 +0100

    Add fetch pending api method

--- source_code/client/src/api/club.js
+++ source_code/client/src/api/club.js
@@ -6,12 +6,20 @@ const urls = {
   get create() {
     return this.root + '/create';
   },
+  get fetchPending() {
+    return this.root + '/pending';
+  },
 };
 
 const create = (params = {}) => {
   return request.post(urls.create, params).then(extractData);
 };
 
+const fetchPending = () => {
+  return request.get(urls.fetchPending).then(extractData);
+};
+
 export default {
   create,
+  fetchPending,
 };
