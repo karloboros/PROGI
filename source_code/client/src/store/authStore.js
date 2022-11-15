60f250aeb21bed5fd04df399d720b922e02ca681
Lnola <nola.luka@gmail.com>
2022-11-15 20:49:41 +0100

    Add is logged in to auth store

--- source_code/client/src/store/authStore.js
+++ source_code/client/src/store/authStore.js
@@ -1,15 +1,19 @@
+import { computed, ref } from 'vue';
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
 import { useStorage } from '@vueuse/core';
 
 const useAuthStore = defineStore('auth', () => {
   const user = ref(useStorage('user', {}));
 
+  const isLoggedIn = computed(() => {
+    return !!user.value.id;
+  });
+
   const setUser = userToSet => {
     user.value = userToSet;
   };
 
-  return { user, setUser };
+  return { user, isLoggedIn, setUser };
 });
 
 export default useAuthStore;
