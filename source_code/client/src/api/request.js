c97a264fa5d1c2e4b25593373a27ee78dd0d95f3
Lnola <nola.luka@gmail.com>
2022-11-15 02:50:35 +0100

    Apply default header to request config

--- source_code/client/src/api/request.js
+++ source_code/client/src/api/request.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import { headers } from './helpers';
 import { StatusCodes } from 'http-status-codes';
 
 const { FORBIDDEN } = StatusCodes;
 
 const config = {
   baseURL: 'http://localhost:3001/api',
-  headers: { 'Content-Type': 'application/json' },
+  headers: headers.default,
 };
 
 const request = axios.create(config);
