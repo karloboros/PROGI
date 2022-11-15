a32f9011468078b102faed7761470725eee0e1e2
Lnola <nola.luka@gmail.com>
2022-11-15 19:38:37 +0100

    Create location model

---
+++ source_code/server/src/location/location.model.ts
@@ -0,0 +1,38 @@
+import { IFields } from 'shared/database/types';
+import { ILocation } from './types';
+import { Model } from 'sequelize';
+
+class LocationModel extends Model implements ILocation {
+  id!: number;
+  name!: string;
+  coordinates?: string;
+
+  static fields({ INTEGER, STRING }: IFields) {
+    return {
+      id: {
+        type: INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: STRING,
+        allowNull: false,
+        unique: true,
+      },
+      coordinates: {
+        type: STRING,
+        allowNull: true,
+      },
+    };
+  }
+
+  static dbOptions() {
+    return {
+      modelName: 'location',
+      tableName: 'locations',
+      timestamps: false,
+    };
+  }
+}
+
+export default LocationModel;
