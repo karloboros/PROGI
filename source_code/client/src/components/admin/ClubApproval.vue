b4bd20285ac68e26e52dd098072b34505328c0c6
Lnola <nola.luka@gmail.com>
2022-11-15 21:35:16 +0100

    Fetch and display pending clubs

--- source_code/client/src/components/admin/ClubApproval.vue
+++ source_code/client/src/components/admin/ClubApproval.vue
@@ -1,3 +1,18 @@
-<template>Club approval</template>
+<template>
+  <n-space align="center" justify="center">
+    <n-card title="Pending clubs" size="huge">
+      <n-p v-for="club in clubs" :key="club.name">{{ club.name }}</n-p>
+    </n-card>
+  </n-space>
+</template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue';
+import { clubApi } from '@/api';
+
+const clubs = ref([]);
+
+onMounted(async () => {
+  clubs.value = await clubApi.fetchPending();
+});
+</script>
