95dffc1e956d6b5ac9195ca94f247f59f03750a5
Lnola <nola.luka@gmail.com>
2022-11-15 01:41:38 +0100

    Add upload middleware to uploadProfileImage method

--- source_code/server/src/user/index.ts
+++ source_code/server/src/user/index.ts
@@ -1,9 +1,25 @@
 import { login, logout, register, uploadProfileImage } from './user.controller';
+import multer from 'multer';
 import { Router } from 'express';
+
+const storage = multer.diskStorage({
+  destination: (_req, _file, cb) => {
+    cb(null, '.tmp');
+  },
+  filename: (_req, _file, cb) => {
+    cb(null, _file.originalname);
+  },
+});
+
+const upload = multer({ storage });
 
 const router = Router();
 const path = '/users';
 
-router.post('/login', login).post('/register', register).post('/logout', logout).post('/upload', uploadProfileImage);
+router
+  .post('/login', login)
+  .post('/register', register)
+  .post('/logout', logout)
+  .post('/upload', upload.single('file'), uploadProfileImage);
 
 export default { router, path };
