0c47233cd252f9390c1aa8d224af9c962fd91637
Lnola <nola.luka@gmail.com>
2022-11-15 19:38:44 +0100

    Create club model

---
+++ source_code/server/src/club/club.model.ts
@@ -0,0 +1,65 @@
+import { ApprovalStatus, IClub } from './types';
+import { IFields } from 'shared/database/types';
+import { Model } from 'sequelize';
+
+class ClubModel extends Model implements IClub {
+  id!: number;
+  name!: string;
+  phone!: string;
+  email!: string;
+  description?: string;
+  approvalStatus!: ApprovalStatus;
+  ownerId!: number;
+  locationId!: number;
+
+  static fields({ INTEGER, STRING, TEXT }: IFields) {
+    return {
+      id: {
+        type: INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: STRING,
+        allowNull: false,
+        unique: true,
+      },
+      phone: {
+        type: STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: STRING,
+        allowNull: false,
+        unique: true,
+      },
+      approvalStatus: {
+        type: INTEGER,
+        allowNull: false,
+      },
+      description: {
+        type: TEXT,
+        allowNull: true,
+      },
+      ownerId: {
+        type: INTEGER,
+        allowNull: false,
+      },
+      locationId: {
+        type: INTEGER,
+        allowNull: false,
+      },
+    };
+  }
+
+  static dbOptions() {
+    return {
+      modelName: 'club',
+      tableName: 'clubs',
+      timestamps: false,
+    };
+  }
+}
+
+export default ClubModel;
