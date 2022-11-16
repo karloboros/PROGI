edaa0e2371a271a1cce084dc64bab17453ac6bfb
Lnola <nola.luka@gmail.com>
2022-11-16 17:03:44 +0100

    Create the pending club datatable

--- source_code/client/src/components/admin/ClubApproval.vue
+++ source_code/client/src/components/admin/ClubApproval.vue
@@ -1,18 +1,69 @@
 <template>
   <n-space align="center" justify="center">
     <n-card title="Pending clubs" size="huge">
-      <n-p v-for="club in clubs" :key="club.name">{{ club.name }}</n-p>
+      <n-data-table :columns="columns" :data="clubs" :bordered="false" />
     </n-card>
   </n-space>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { h, onMounted, ref } from 'vue';
 import { clubApi } from '@/api';
+import { NButton } from 'naive-ui';
+
+const ApproveButton = club => {
+  return h(
+    NButton,
+    {
+      secondary: true,
+      type: 'primary',
+      size: 'small',
+      onClick: () => approve(club),
+    },
+    { default: () => 'Approve' },
+  );
+};
+
+const RejectButton = club => {
+  return h(
+    NButton,
+    {
+      secondary: true,
+      type: 'error',
+      size: 'small',
+      onClick: () => reject(club),
+    },
+    { default: () => 'Reject' },
+  );
+};
+
+const columns = [
+  { title: 'Name', key: 'name' },
+  { title: 'Email', key: 'email' },
+  { title: 'Phone', key: 'phone' },
+  { title: 'Description', key: 'description' },
+  { title: 'Owner', key: 'ownerName' },
+  { title: 'Address', key: 'locationName' },
+  { title: 'Approve', key: 'approve', render: ApproveButton },
+  { title: 'Reject', key: 'reject', render: RejectButton },
+];
 
 const clubs = ref([]);
 
 onMounted(async () => {
-  clubs.value = await clubApi.fetchPending();
+  const data = await clubApi.fetchPending();
+  clubs.value = data.map(club => ({
+    ...club,
+    ownerName: club.owner.fullName,
+    locationName: club.location.name,
+  }));
 });
+
+const approve = ({ id }) => {
+  console.log(id);
+};
+
+const reject = ({ id }) => {
+  console.log(id);
+};
 </script>
