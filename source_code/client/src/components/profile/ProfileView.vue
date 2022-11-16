69b7e865ad5c46da720b2e934a7278ce9b409a00
Lnola <nola.luka@gmail.com>
2022-11-16 19:21:33 +0100

    Introduce remove user account

--- source_code/client/src/components/profile/ProfileView.vue
+++ source_code/client/src/components/profile/ProfileView.vue
@@ -1,5 +1,45 @@
 <template>
-  <n-space align="center" justify="center" item-style="width: 80%"> Profile </n-space>
+  <n-space class="club-approval" align="center" justify="center" item-style="width: 80%">
+    <n-card title="Profile" size="huge">
+      <template #header-extra>
+        <n-button @click="confirm" type="error">Delete account</n-button>
+      </template>
+      <n-skeleton v-if="loading" text :repeat="6" />
+    </n-card>
+  </n-space>
 </template>
 
-<script setup></script>
+<script setup>
+import { useDialog, useMessage } from 'naive-ui';
+import { authApi } from '@/api';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const loading = ref(true);
+
+const router = useRouter();
+const message = useMessage();
+const dialog = useDialog();
+
+const confirm = () => {
+  dialog.warning({
+    title: 'Confirm',
+    content: 'Are you sure? you want to delete your account. This is irreversible!',
+    positiveText: 'Yes',
+    negativeText: 'No',
+    onPositiveClick: remove,
+  });
+};
+
+const remove = async () => {
+  try {
+    await authApi.remove();
+    message.error('Successfully deleted');
+    router.push({ name: 'Auth' });
+  } catch (err) {
+    message.error(err.response.data.message);
+  } finally {
+    loading.value = false;
+  }
+};
+</script>
