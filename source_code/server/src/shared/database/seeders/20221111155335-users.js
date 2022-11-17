ac9b4f93d5ba6af3d23b48607089d80ee5da31da
Lnola <nola.luka@gmail.com>
2022-11-17 01:28:19 +0100

    Fill user seed with role names as usernames

--- source_code/server/src/shared/database/seeders/20221111155335-users.js
+++ source_code/server/src/shared/database/seeders/20221111155335-users.js
@@ -3,17 +3,26 @@ const bcrypt = require('bcrypt');
 
 const TABLE_NAME = 'users';
 
-const createUser = (index, role = 3) => {
+const createUser = (index, data = {}) => {
   const password = bcrypt.hashSync('123', process.env.SALT_ROUNDS || 12);
+  const {
+    username = `example${index}`,
+    firstName = `test`,
+    lastName = `user${index}`,
+    phone = `098765432${index}`,
+    email = `example${index}@gmail.com`,
+    role = 3,
+  } = data;
+
   return {
-    username: `example${index}`,
-    firstName: `test`,
-    lastName: `user ${index}`,
+    username,
+    firstName,
+    lastName,
     password,
     gender: 0,
     dateOfBirth: new Date('2001'),
-    phone: `098765432${index}`,
-    email: `example${index}@gmail.com`,
+    phone,
+    email,
     role,
     experienceDescription: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
   };
@@ -21,8 +30,12 @@ const createUser = (index, role = 3) => {
 
 module.exports = {
   up(queryInterface) {
-    const users = [createUser('', 0)];
-    for (let i = 1; i < 4; i++) users.push(createUser(i, i));
+    const users = [createUser('')];
+    for (let i = 1; i < 5; i++) users.push(createUser(i));
+    users.push(createUser(10, { username: 'admin', firstName: 'Admin', lastName: 'Adminius', role: 0 }));
+    users.push(createUser(11, { username: 'owner', firstName: 'Owner', lastName: 'Ownius', role: 1 }));
+    users.push(createUser(12, { username: 'trainer', firstName: 'Trainer', lastName: 'Trainius', role: 2 }));
+    users.push(createUser(13, { username: 'user', firstName: 'User', lastName: 'Usius', role: 3 }));
     return queryInterface.bulkInsert(TABLE_NAME, users);
   },
   down(queryInterface) {
