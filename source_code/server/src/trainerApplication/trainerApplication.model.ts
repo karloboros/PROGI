845963a0982020eab02736095b81072ec74cfd4e
karloboros <karlo.boros1@gmail.com>
2022-12-09 21:27:53 +0100

    add model for trainerApplication

---
+++ source_code/server/src/trainerApplication/trainerApplication.model.ts
@@ -0,0 +1,76 @@
+import { ApprovalStatus, IClub } from 'club/types';
+import { IFields, IModels } from 'shared/database/types';
+import { ITrainerApplication } from './types';
+import { Model } from 'sequelize';
+
+class TrainerApplicationModel extends Model implements ITrainerApplication {
+  id!: number;
+  motivationalLetter!: string;
+  certificate!: string;
+  status!: ApprovalStatus;
+  trainerId!: number;
+  clubId!: IClub;
+
+  static fields({ INTEGER, STRING }: IFields) {
+    return {
+      id: {
+        type: INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      motvivationalLetter: {
+        type: STRING,
+        allowNull: false,
+      },
+      certificate: {
+        type: STRING,
+        allowNull: false,
+      },
+      status: {
+        type: INTEGER,
+        allowNull: false,
+      },
+      trainerId: {
+        type: INTEGER,
+        allowNull: false,
+      },
+      clubId: {
+        type: INTEGER,
+        allowNull: false,
+      },
+    };
+  }
+
+  static associate({ Club, User }: IModels) {
+    this.belongsTo(User, {
+      foreignKey: { name: 'ownerId', field: 'ownerId' },
+    });
+    this.belongsTo(Club, {
+      foreignKey: { name: 'clubId', field: 'clubId' },
+    });
+  }
+
+  static scopes() {
+    return {
+      includeOwner: {
+        include: ['userId'],
+      },
+      includeClub: {
+        include: ['clubId'],
+      },
+      pending: {
+        where: { status: ApprovalStatus.Pending },
+      },
+    };
+  }
+
+  static dbOptions() {
+    return {
+      modelName: 'trainerApplication',
+      tableName: 'trainerApplications',
+      timestamps: false,
+    };
+  }
+}
+
+export default TrainerApplicationModel;
