083fb94749712936aae9ca2e6b9f1576cc390537
karloboros <karlo.boros1@gmail.com>
2022-12-10 20:23:06 +0100

    made getter for test function

--- source_code/server/src/trainerApplication/index.ts
+++ source_code/server/src/trainerApplication/index.ts
@@ -1,16 +1,9 @@
-import { create, fetchPending, updateApprovalStatus } from './trainerApplication.controller';
-import authenticate from 'shared/auth/authenticate';
-import refresh from 'shared/auth/refresh';
 import { Router } from 'express';
+import { test } from './trainerApplication.controller';
 
 const router = Router();
 const path = '/trainerApplications';
 
-router
-  .use(authenticate)
-  .use(refresh)
-  .post('/create', create)
-  .get('/pending', fetchPending)
-  .post('/update-approval', updateApprovalStatus);
+router.get('/test', test);
 
 export default { router, path };
