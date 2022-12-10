b5b702f769eed8c10bd97f537188f96d07e2582d
LDomic05 <lucijadomicsplit@gmail.com>
2022-12-10 09:25:20 +0100

    added IModels: BallDance, Dance, DanceBallDance

--- source_code/server/src/shared/database/types.ts
+++ source_code/server/src/shared/database/types.ts
@@ -1,7 +1,10 @@
 import { DataType } from 'sequelize';
 
 // eslint-disable-next-line sort-imports
+import BallDanceModel from 'event/event.model';
 import ClubModel from 'club/club.model';
+import DanceBallDanceModel from 'eventDance/eventDance.model';
+import DanceModel from 'dance/dance.models';
 import LocationModel from 'location/location.model';
 import UserModel from 'user/user.model';
 
@@ -18,6 +21,9 @@ type IModels = {
   Club: typeof ClubModel;
   Location: typeof LocationModel;
   User: typeof UserModel;
+  BallDance: typeof BallDanceModel;
+  Dance: typeof DanceModel;
+  DanceBallDance: typeof DanceBallDanceModel;
 };
 
 export { IFields, IModels };
