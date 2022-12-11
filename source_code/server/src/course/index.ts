60dea89d0e3e7c0e7cc7301a563f71f77ce12ca4
laradjakovic229 <laradjakovic00@gmail.com>
2022-12-11 21:58:32 +0100

    fixed /course/index.ts

--- source_code/server/src/course/index.ts
+++ source_code/server/src/course/index.ts
@@ -1,11 +1,9 @@
-import authenticate from 'shared/auth/authenticate';
-import refresh from 'shared/auth/refresh';
 import { Router } from 'express';
 import { test } from './course.controller';
 
 const router = Router();
 const path = '/course';
 
-router.use(authenticate).use(refresh).use(test);
+router.get('/test', test);
 
 export default { router, path };
