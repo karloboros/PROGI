85fb739045d9d27102398626514f5aa2f95f65a8
Ana Vrabec <ana.vrabec@estudent.hr>
2022-12-19 23:47:51 +0100

    add routes for dance controller

--- source_code/server/src/dance/index.ts
+++ source_code/server/src/dance/index.ts
@@ -1,9 +1,30 @@
+import { create, edit, fetchAll, remove, uploadDanceImage } from './dance.controller';
+import authenticate from 'shared/auth/authenticate';
+import multer from 'multer';
+import refresh from 'shared/auth/refresh';
 import { Router } from 'express';
-import { test } from './dance.controller';
+
+const storage = multer.diskStorage({
+  destination: (_req, _file, cb) => {
+    cb(null, '.tmp/images');
+  },
+  filename: (_req, file, cb) => {
+    cb(null, Date.now() + '_' + file.originalname);
+  },
+});
 
 const router = Router();
 const path = '/dances';
 
-router.get('/test', test);
+const upload = multer({ storage });
+
+router
+  .use(authenticate)
+  .use(refresh)
+  .post('/create', create)
+  .get('/all', fetchAll)
+  .post('/upload', upload.single('file'), uploadDanceImage)
+  .post('/edit/:id', edit)
+  .delete('/:id', remove);
 
 export default { router, path };
