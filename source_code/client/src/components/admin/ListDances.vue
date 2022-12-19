b44479819372d614d4aae34ab4effa1efc3de25f
Ana Vrabec <ana.vrabec@estudent.hr>
2022-12-20 00:09:51 +0100

    add view for listing all dances

---
+++ source_code/client/src/components/admin/ListDances.vue
@@ -0,0 +1,64 @@
+<template>
+  <n-space class="view-dances" align="center" justify="center" item-style="width: 80%">
+    <n-card title="All dances" size="huge">
+      <n-skeleton v-if="loading" text :repeat="6" />
+      <n-data-table v-else :columns="columns" :data="dances" :bordered="false" />
+    </n-card>
+  </n-space>
+</template>
+
+<script setup>
+import { h, onMounted, ref } from 'vue';
+import { danceApi } from '@/api';
+import { NButton } from 'naive-ui';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const EditButton = dance => {
+  return h(
+    NButton,
+    {
+      secondary: true,
+      type: 'primary',
+      size: 'small',
+      onClick: () => viewDanceDetails(dance.id),
+    },
+    { default: () => 'Edit' },
+  );
+};
+
+const columns = [
+  { title: 'Name', key: 'name' },
+  { title: 'Description', key: 'description' },
+  { title: 'Image', key: 'image' },
+  { title: 'VIdeo url', key: 'videoUrl' },
+  { title: 'Edit', key: 'edit', render: EditButton },
+];
+
+const dances = ref([]);
+const loading = ref(true);
+
+onMounted(async () => {
+  const data = await danceApi.fetchAll();
+  dances.value = data.map(dance => ({
+    ...dance,
+  }));
+
+  loading.value = false;
+});
+
+const viewDanceDetails = id => {
+  router.push({
+    name: 'Edit dance',
+    params: { id },
+  });
+};
+</script>
+
+<style scoped>
+.view-dances ::v-deep(.n-data-table-th),
+.view-dances ::v-deep(.n-data-table-td) {
+  white-space: nowrap;
+}
+</style>
