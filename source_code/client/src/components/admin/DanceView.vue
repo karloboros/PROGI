dd7316fb631c32e03942a3530de3f6f0c991b428
Ana Vrabec <ana.vrabec@estudent.hr>
2022-12-20 01:54:52 +0100

    add view for deleting and updating dances, still have to add button functionality

---
+++ source_code/client/src/components/admin/DanceView.vue
@@ -0,0 +1,55 @@
+<template>
+  <n-space class="club-approval" align="center" justify="center" item-style="width: 80%">
+    <n-card title="Dance" size="huge">
+      <template #header-extra>
+        <n-button @click="confirm" type="error">Delete dance</n-button>
+      </template>
+      <dance-add :initial-values="dance" />
+    </n-card>
+  </n-space>
+</template>
+
+<script setup>
+import { onBeforeMount, ref } from 'vue';
+import { useDialog, useMessage } from 'naive-ui';
+import DanceAdd from '@/components/admin/AddDance.vue';
+import { danceApi } from '@/api';
+import { useRoute } from 'vue-router';
+
+const message = useMessage();
+const dialog = useDialog();
+
+const route = useRoute();
+const id = route.params.id;
+
+// fix editing and deleting - it doesn't work yet
+const dance = ref([]);
+
+onBeforeMount(async () => {
+  const data = await danceApi.fetchById(id);
+  dance.value = data.map(dance => ({
+    ...dance,
+  }));
+  console.log(dance.value);
+});
+console.log(dance.value);
+
+const confirm = () => {
+  dialog.error({
+    title: 'Confirm',
+    content: 'Are you sure you want to delete this dance. This is irreversible!',
+    positiveText: 'Yes',
+    negativeText: 'No',
+    onPositiveClick: remove,
+  });
+};
+
+const remove = async () => {
+  try {
+    await danceApi.remove(id);
+    message.success('Successfully deleted');
+  } catch (err) {
+    message.error(err.response?.data.message || err);
+  }
+};
+</script>
