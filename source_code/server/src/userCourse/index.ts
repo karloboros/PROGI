b4a3a05deea37a558011d589ab746ac976b3fea7
ninaduric7 <ninaduric83@gmail.com>
2022-12-21 13:13:12 +0100

    updated index.ts for usercourse

--- source_code/server/src/userCourse/index.ts
+++ source_code/server/src/userCourse/index.ts
@@ -1,10 +1,20 @@
-import { test1, test2 } from './userCourse.controller';
+import {
+  acceptCandidatesForCourse,
+  denyCandidatesForCourse,
+  getCandidatesForCourse,
+  seeYourCourseApplications,
+  sendApplicationForCourse,
+} from './userCourse.controller';
+
 import { Router } from 'express';
 
 const router = Router();
 const path = '/user-courses';
 
-router.get('/test1', test1);
-router.get('/test2', test2);
+router.get('/get-candidate/:courseId', getCandidatesForCourse);
+router.get('/accept-candidate/:id', acceptCandidatesForCourse);
+router.get('/deny-candidate/:id', denyCandidatesForCourse);
+router.get('/see-applications/:userId', seeYourCourseApplications);
+router.get('/apply-for-course/:userId/:courseId', sendApplicationForCourse);
 
 export default { router, path };
