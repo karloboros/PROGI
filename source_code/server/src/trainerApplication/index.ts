be2a8db53e8a83bf2643091c6c619b9d08ba8f33
ninaduric7 <ninaduric83@gmail.com>
2022-12-21 13:14:18 +0100

    updated index.ts for trainerapplication

--- source_code/server/src/trainerApplication/index.ts
+++ source_code/server/src/trainerApplication/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { test } from './trainerApplication.controller';
+// eslint-disable-next-line sort-imports
+import { acceptApplication, denyApplication, sendApplication } from './trainerApplication.controller';
 
 const router = Router();
 const path = '/trainer-applications';
 
-router.get('/test', test);
+router.post('/send-application/:userId/:clubId', sendApplication);
+router.get('/accept-application/:id', acceptApplication);
+router.get('/deny-application/:id', denyApplication);
 
 export default { router, path };
