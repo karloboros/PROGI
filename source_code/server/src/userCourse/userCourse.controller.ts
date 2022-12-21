e3a1c21ab0a5a15caece27735499717f8ebf8574
ninaduric7 <ninaduric83@gmail.com>
2022-12-21 13:11:55 +0100

    added userCourse controller

--- source_code/server/src/userCourse/userCourse.controller.ts
+++ source_code/server/src/userCourse/userCourse.controller.ts
@@ -1,13 +1,68 @@
+// eslint-disable-next-line sort-imports
+import { CREATED } from 'http-status';
+// eslint-disable-next-line sort-imports
 import { Request, Response } from 'express';
 import { UserCourse } from 'shared/database';
 
-const test1 = async (req: Request, res: Response) => {
-  const userCourses = await UserCourse.findAll({ include: 'user' });
-  return res.send(userCourses);
+const getCandidatesForCourse = async (req: Request, res: Response) => {
+  try {
+    const candidates = await UserCourse.findAll({ where: { courseId: req.params.courseId } });
+    return res.json({ ...candidates });
+  } catch {
+    return res.status(404);
+  }
 };
-const test2 = async (req: Request, res: Response) => {
-  const userCourses = await UserCourse.findAll({ include: 'course' });
-  return res.send(userCourses);
+const acceptCandidatesForCourse = async (req: Request, res: Response) => {
+  try {
+    const candidate = await UserCourse.findOne({ where: { id: req.params.id } });
+    if (!candidate) return res.status(404).send('Nema takvog UserCoursea');
+    candidate.status = 1;
+    await candidate.save();
+    return res.send('Kandidat prihvaćen na tečaj!');
+  } catch {
+    return res.status(404);
+  }
 };
 
-export { test1, test2 };
+const denyCandidatesForCourse = async (req: Request, res: Response) => {
+  try {
+    const candidate = await UserCourse.findOne({ where: { id: req.params.id } });
+    if (!candidate) return res.status(404).send('Nema takvog UserCoursea');
+    candidate.status = 2;
+    await candidate.save();
+    return res.send('Kandidat uklonjen sa tečaja!');
+  } catch {
+    return res.status(404);
+  }
+};
+
+const seeYourCourseApplications = async (req: Request, res: Response) => {
+  try {
+    const applications = await UserCourse.findAll({ where: { userId: req.params.userId } });
+    if (!applications) return res.send('Trenutno nemate aktivnih prijava na tečajeve!');
+    return res.json({ ...applications });
+  } catch {
+    return res.status(404);
+  }
+};
+
+const sendApplicationForCourse = async (req: Request, res: Response) => {
+  try {
+    const application = {
+      status: 0,
+      userId: req.params.userId,
+      courseId: req.params.courseId,
+    };
+    UserCourse.create(application);
+    return res.status(CREATED).send('Uspješno ste poslali prijavu za tečaj!');
+  } catch {
+    return res.status(404);
+  }
+};
+export {
+  getCandidatesForCourse,
+  acceptCandidatesForCourse,
+  denyCandidatesForCourse,
+  seeYourCourseApplications,
+  sendApplicationForCourse,
+};
