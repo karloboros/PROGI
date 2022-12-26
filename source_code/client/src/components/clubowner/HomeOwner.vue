084c90aead7ed336163b4f49a4897f2f838a85fa
laradjakovic229 <laradjakovic00@gmail.com>
2022-12-26 16:46:44 +0100

    update buttons

--- source_code/client/src/components/clubowner/HomeOwner.vue
+++ source_code/client/src/components/clubowner/HomeOwner.vue
@@ -3,25 +3,16 @@
     <n-modal v-model:show="showModal">
       <course-create @created="showModal = false" />
     </n-modal>
-    <n-modal v-model:show="showModal2">
-      <course-edit @created="showModal2 = false" />
-    </n-modal>
-    <n-modal v-model:show="showModal2">
-      <courses-show @created="showModal3 = false" />
-    </n-modal>
     <n-button @click="showModal = true">Create course</n-button>
-    <n-button @click="showModal2 = true">Edit course</n-button>
     <n-button @click="router.push({ name: 'Courses' })">My courses</n-button>
   </n-space>
 </template>
 
 <script setup>
 import CourseCreate from './CourseCreate.vue';
-import CourseEdit from './CourseEdit.vue';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const showModal = ref(false);
-const showModal2 = ref(false);
 const router = useRouter();
 </script>
