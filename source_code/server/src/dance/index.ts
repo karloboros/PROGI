c2876897fb62ece5c833161547506be1bbf5fea8
Ana Vrabec <ana.vrabec@estudent.hr>
2022-12-30 01:37:04 +0100

    remove unused imports

--- source_code/server/src/dance/index.ts
+++ source_code/server/src/dance/index.ts
@@ -1,7 +1,5 @@
 import { create, edit, fetchAll, fetchById, remove, uploadDanceImage } from './dance.controller';
-import authenticate from 'shared/auth/authenticate';
 import multer from 'multer';
-import refresh from 'shared/auth/refresh';
 import { Router } from 'express';
 
 const storage = multer.diskStorage({
@@ -19,8 +17,6 @@ const path = '/dances';
 const upload = multer({ storage });
 
 router
-  .use(authenticate)
-  .use(refresh)
   .post('/create', create)
   .get('/all', fetchAll)
   .post('/upload', upload.single('file'), uploadDanceImage)
