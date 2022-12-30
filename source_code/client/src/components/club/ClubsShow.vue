e80cc4019a235b29692b2a1f5b683295172652b0
laradjakovic229 <laradjakovic00@gmail.com>
2022-12-30 13:20:13 +0100

    clubsShow template, still need to add router

---
+++ source_code/client/src/components/club/ClubsShow.vue
@@ -0,0 +1,43 @@
+<template>
+  <n-space class="club-approval" align="center" justify="center" item-style="width: 80%">
+    <n-card title="My courses" size="huge">
+      <n-data-table :columns="columns" :data="clubs" :pagination="pagination" :bordered="false" />
+    </n-card>
+  </n-space>
+</template>
+
+<script setup>
+// import { h, onMounted, ref } from 'vue';
+import { h } from 'vue';
+import { NButton } from 'naive-ui';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const ShowCoursesButton = club => {
+  return h(
+    NButton,
+    {
+      secondary: true,
+      type: 'primary',
+      size: 'small',
+      onClick: () => viewCourseDetails(club.id),
+    },
+    { default: () => 'See Courses' },
+  );
+};
+
+const columns = [
+  { title: 'Club', key: 'name' },
+  { title: 'Show courses', key: 'show', render: ShowCoursesButton },
+];
+
+const clubs = [{ name: 'Klub1' }, { name: 'Klub2' }];
+
+const viewCourseDetails = id => {
+  router.push({
+    name: 'CoursesShow',
+    params: { id },
+  });
+};
+</script>
