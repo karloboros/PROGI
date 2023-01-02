0d9f24f94dccd5214c2a66cb8f02eb08ae485b6b
LDomic05 <lucijadomicsplit@gmail.com>
2023-01-02 19:48:33 +0100

    add for map functionality

--- source_code/client/src/constants/index.js
+++ source_code/client/src/constants/index.js
@@ -1,3 +1,4 @@
 export { default as ApprovalStatus } from './approvalStatus';
 export { default as Gender } from './gender';
 export { default as Role } from './role';
+export { attribution, mapBackground, view } from './map';
