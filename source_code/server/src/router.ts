92f3cc5739c1f48e7290e32fcbdb1886776e3a7f
laradjakovic229 <laradjakovic00@gmail.com>
2023-01-04 00:14:03 +0100

    add course to router

--- source_code/server/src/router.ts
+++ source_code/server/src/router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 // eslint-disable-next-line sort-imports
 import club from 'club';
+import course from 'course';
 import dance from 'dance';
 import event from 'event';
 import eventDance from 'eventDance';
@@ -12,6 +13,7 @@ import userCourse from 'userCourse';
 const router = Router();
 
 router.use(club.path, club.router);
+router.use(course.path, course.router);
 router.use(dance.path, dance.router);
 router.use(event.path, event.router);
 router.use(eventDance.path, eventDance.router);
