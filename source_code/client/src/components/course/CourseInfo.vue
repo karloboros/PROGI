e43213b69be5f6d1ff27e455bbaf24b5f7aa318b
laradjakovic229 <laradjakovic00@gmail.com>
2023-01-04 00:14:18 +0100

    course info page

---
+++ source_code/client/src/components/course/CourseInfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <n-space class="club-approval" align="center" justify="center" item-style="width: 90%">
+    <n-card title="Course info" size="huge">
+      <div>
+        <h2>{{ course.name }}</h2>
+        <p>Lokacija: {{ course.locationName }}</p>
+        <p>Ples: {{ course.danceName }}</p>
+        <p>Kapacitet polaznika: {{ course.capacity }}</p>
+        <p>{{ course.description }}</p>
+      </div>
+    </n-card>
+  </n-space>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+// import { courseApi } from '@/api';
+
+const course = ref();
+
+const getInfo = async () => {
+  // course.value = await courseApi.fetchById();
+  course.value = {
+    name: 'Ples neki',
+    locationName: 'FER',
+    danceName: 'Samba',
+    description: 'Plesat ćemo sambu i učiti nešto novo blabla.',
+    capacity: 20,
+  };
+};
+
+onMounted(() => {
+  getInfo();
+});
+</script>
