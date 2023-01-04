6d0608e4f37857cfe70b50dfc17d3a8a79d2057c
laradjakovic229 <laradjakovic00@gmail.com>
2023-01-04 17:29:16 +0100

    course info

--- source_code/client/src/components/course/CourseInfo.vue
+++ source_code/client/src/components/course/CourseInfo.vue
@@ -1,12 +1,20 @@
 <template>
-  <n-space class="club-approval" align="center" justify="center" item-style="width: 90%">
+  <n-space class="club-approval" align="center" justify="center" item-style="width: 60%">
     <n-card title="Course info" size="huge">
       <div>
         <h2>{{ course.name }}</h2>
+        <p>Klub: {{ course.clubName }}</p>
         <p>Lokacija: {{ course.locationName }}</p>
         <p>Ples: {{ course.danceName }}</p>
-        <p>Kapacitet polaznika: {{ course.capacity }}</p>
+        <p>Trener: {{ course.trainerName }}</p>
+        <p v-if="course.capacity">Kapacitet polaznika: {{ course.capacity }}</p>
+        <p v-if="course.minAge">Min dob: {{ course.minAge }}</p>
+        <p v-if="course.maxAge">Max dobi: {{ course.maxAge }}</p>
+        <p v-if="course.gender">Spol: {{ course.gender }}</p>
+        <p>Krajnji rok prijave: {{ course.applicationDeadline }}</p>
+        <p>Opis:</p>
         <p>{{ course.description }}</p>
+        <p v-if="course.additionalRules">Dodatne infomacije: {{ course.additionalRules }}</p>
       </div>
     </n-card>
   </n-space>
@@ -14,22 +22,39 @@
 
 <script setup>
 import { onMounted, ref } from 'vue';
-// import { courseApi } from '@/api';
+import { courseApi } from '@/api';
+import { useRoute } from 'vue-router';
 
-const course = ref();
+const course = ref([]);
+const route = useRoute();
+const id = route.params.id;
 
-const getInfo = async () => {
-  // course.value = await courseApi.fetchById();
+onMounted(async () => {
+  const data = await courseApi.fetchById(id);
   course.value = {
-    name: 'Ples neki',
-    locationName: 'FER',
-    danceName: 'Samba',
-    description: 'Plesat ćemo sambu i učiti nešto novo blabla.',
-    capacity: 20,
+    id: data.id,
+    name: data.name,
+    description: data.description,
+    capacity: data.capacity,
+    minAge: data.minAge,
+    maxAge: data.maxAge,
+    gender: data.gender,
+    applicationDeadline: new Date(data.applicationDeadline).toLocaleDateString(),
+    additionalRules: data.additionalRules,
+    clubName: data.club.name,
+    danceName: data.dance.name,
+    locationName: data.location.name,
+    trainerName: 'ime',
   };
-};
-
-onMounted(() => {
-  getInfo();
 });
+
+/*
+const course = {
+  name: 'Ples neki',
+  locationName: 'FER',
+  danceName: 'Samba',
+  description: 'Plesat ćemo sambu i učiti nešto novo blabla.',
+  capacity: 20,
+};
+*/
 </script>
