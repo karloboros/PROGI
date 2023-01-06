a9514074200a1bf2a5e5afed0551a8a10ec80608
LDomic05 <lucijadomicsplit@gmail.com>
2023-01-06 15:04:20 +0100

    add connection with Club model when fetch Event

--- source_code/server/src/event/event.controller.ts
+++ source_code/server/src/event/event.controller.ts
@@ -1,13 +1,6 @@
 import { Club, Event, Location } from 'shared/database';
 import { Request, Response } from 'express';
 
-const fetchClubNames = async (req: Request, res: Response) => {
-  const pendingClubNames = await Club.findAll({
-    attributes: ['id', 'name'],
-  });
-  return res.send(pendingClubNames);
-};
-
 const fetchEventLocation = async (req: Request, res: Response) => {
   const events = await Event.findAll({
     include: [{ model: Club }, { model: Location }],
@@ -15,4 +8,4 @@ const fetchEventLocation = async (req: Request, res: Response) => {
   return res.send(events);
 };
 
-export { fetchClubNames, fetchEventLocation };
+export { fetchEventLocation };
