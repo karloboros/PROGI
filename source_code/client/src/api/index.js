3fe370471ae88ac988893b852db8af2d1b6559de
laradjakovic229 <laradjakovic00@gmail.com>
2023-01-06 18:39:38 +0100

    rename lessonApi

--- source_code/client/src/api/index.js
+++ source_code/client/src/api/index.js
@@ -2,5 +2,5 @@ export { default as authApi } from './auth';
 export { default as courseApi } from './course';
 export { default as clubApi } from './club';
 export { default as danceApi } from './dance';
-export { default as LessonApi } from './lessson';
+export { default as lessonApi } from './lessson';
 export { default as trainerApplicationApi } from './trainer';
