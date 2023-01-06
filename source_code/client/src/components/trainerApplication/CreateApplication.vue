3af7a4a868acda438eac7d5429d4566139b8f035
karloboros <karlo.boros1@gmail.com>
2023-01-06 18:19:13 +0100

    add view for application for trainers
    The work is not finished!

---
+++ source_code/client/src/components/trainerApplication/CreateApplication.vue
@@ -0,0 +1,90 @@
+<template>
+    <n-space class="create-application" align="center" justify="center" item-style="width: 80%">
+      <n-form ref="formRef" @submit.prevent="submit" :model="values" :rules="rules">
+        <n-form-item label="Motivational letter" path="motivationalLetter">
+            <n-input v-model:value="values.motivationalLetter" type="textarea" placeholder="I want to..." />
+        </n-form-item>
+        <n-form-item label="Certificate" path="certificate">
+            <input type="file" accept="application/pdf" class="fileinput" @change="onChange">
+        </n-form-item>
+        
+        <n-form-item>
+            <n-button type="primary" attr-type="submit">Send application</n-button>
+        </n-form-item>
+    </n-form>
+</n-space>
+</template>
+
+methods: {
+async onChange(e) {
+    let file = e.target.files[0];
+    let formData = new FormData();
+    formData.append('pdf', file);
+
+    await axios.post(`/assets/${this.certificate}`, formData)
+        .then( res => console.log(res.data) )
+},
+}
+
+<script setup>
+  // eslint-disable-next-line sort-imports
+  import { trainerApplicationApi, clubApi, authApi } from '@/api';
+  import { onMounted, ref } from 'vue';
+  //import { computed } from '@vue/reactivity';
+  import { useMessage } from 'naive-ui';
+  import { useRoute } from 'vue-router';    
+  import { validationRules } from '@/utils';
+  
+  const emit = defineEmits(['sent']);
+  
+  
+  const props = defineProps({
+      initialValues: {
+          type: Object,
+          default: () => ({
+              motivationalLetter: '',
+              certificate: '',
+            }),
+        },
+    });
+
+  /*  
+  onMounted(async () => {
+    clubs.value = await clubApi.fetchAll();
+    console.log(clubs.value);
+    clubs.value = clubs.value.map(v => ({
+        label: v.name,
+        value: v.name,
+    }));
+  });
+*/    
+    const { required } = validationRules;
+    const rules = {
+        motivationalLetter: required,
+//        certificate: required,
+    };
+    const message = useMessage();
+    const route = useRoute();
+    const values = ref(props.initialValues);
+    const formRef = ref(null);
+    console.log(route.params.id);
+
+    const submit = async () => {
+        formRef.value?.validate(async errors => {
+            if (!errors) {
+                try {
+                await trainerApplicationApi.send({ ...values.value, id: route.params.id, clubId: route.params.clubId });  
+                    message.success('Success');
+                    emit('Sent');
+                } catch (err) {
+                    message.error(err.message);
+                }
+            }
+        });
+    };
+  
+  // eslint-disable-next-line no-unused-vars
+  /* const error = error => {
+    message.error(error);
+  }; */
+  </script>
