e83a94a6fab92847c4ee6e2e66ce84f2afbd8f4c
Nina Đurić <ninaduric83@gmail.com>
2023-01-07 00:32:38 +0000

    add controllers

--- source_code/server/src/userCourse/userCourse.controller.ts
+++ source_code/server/src/userCourse/userCourse.controller.ts
@@ -1,13 +1,78 @@
-import { Request, Response } from 'express';
-import { UserCourse } from 'shared/database';
+import { BAD_REQUEST, CREATED, FORBIDDEN, NOT_FOUND, OK } from 'http-status';
+import { Club, User, UserCourse } from 'shared/database';
+import { NextFunction, Request, Response } from 'express';
+import { ApprovalStatus } from 'club/types';
+import errorMessages from 'shared/constants/errorMessages';
+import HttpError from 'shared/error/httpError';
+import { Role } from 'user/types';
 
-const test1 = async (req: Request, res: Response) => {
-  const userCourses = await UserCourse.findAll({ include: 'user' });
-  return res.send(userCourses);
-};
-const test2 = async (req: Request, res: Response) => {
-  const userCourses = await UserCourse.findAll({ include: 'course' });
-  return res.send(userCourses);
+const apply = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { user } = req;
+    const foundUser = await User.findOne({ where: { id: user.id } });
+    if (!foundUser) return next(new HttpError(NOT_FOUND, errorMessages.NOT_FOUND));
+    if (foundUser.role !== Role.User) return next(new HttpError(FORBIDDEN, errorMessages.FORBIDDEN));
+
+    const { courseId } = req.params;
+    const application = {
+      status: ApprovalStatus.Pending,
+      userId: user.id,
+      courseId,
+    };
+    UserCourse.create(application);
+    return res.status(CREATED).send('Application sent successfully!');
+  } catch {
+    return next(new HttpError(BAD_REQUEST, errorMessages.BAD_REQUEST));
+  }
 };
 
-export { test1, test2 };
+const updateStatus = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const owner = req.user;
+    const { id, isApproved } = req.body;
+    const userCourse = await UserCourse.scope('includeCourse').findByPk(id);
+    if (!userCourse) return next(new HttpError(NOT_FOUND, errorMessages.NOT_FOUND));
+
+    const club = await Club.findByPk(userCourse.course?.clubId);
+    if (!club) return next(new HttpError(NOT_FOUND, errorMessages.NOT_FOUND));
+    if (club.ownerId !== owner.id) return next(new HttpError(FORBIDDEN, errorMessages.FORBIDDEN));
+    userCourse.status = isApproved ? ApprovalStatus.Approved : ApprovalStatus.Rejected;
+    await userCourse.save();
+
+    return res.status(OK).send('Application status changed successfully!');
+  } catch {
+    return next(new HttpError(BAD_REQUEST, errorMessages.BAD_REQUEST));
+  }
+};
+
+const getApproved = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { courseId } = req.params;
+    const userCourses = await UserCourse.scope(['accepted', 'includeUser']).findAll({ where: { courseId } });
+    return res.status(OK).json({ ...userCourses });
+  } catch {
+    return next(new HttpError(BAD_REQUEST, errorMessages.BAD_REQUEST));
+  }
+};
+
+const getPending = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { courseId } = req.params;
+    const userCourses = await UserCourse.scope(['pending', 'includeUser']).findAll({ where: { courseId } });
+    return res.status(OK).json({ ...userCourses });
+  } catch {
+    return next(new HttpError(BAD_REQUEST, errorMessages.BAD_REQUEST));
+  }
+};
+
+const getByUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { userId } = req.params;
+    const applications = await UserCourse.findAll({ where: { userId } });
+    return res.status(OK).json({ ...applications });
+  } catch {
+    return next(new HttpError(BAD_REQUEST, errorMessages.BAD_REQUEST));
+  }
+};
+
+export { apply, updateStatus, getApproved, getPending, getByUser };
