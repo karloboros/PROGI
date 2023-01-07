ccffd05202623e8babe07156c721e0930c6fb0d4
karloboros <karlo.boros1@gmail.com>
2023-01-07 23:21:45 +0100

    small changes in order

--- source_code/client/src/components/trainerApplication/CreateApplication.vue
+++ source_code/client/src/components/trainerApplication/CreateApplication.vue
@@ -1,79 +1,76 @@
 <template>
-    <n-space class="create-application" align="center" justify="center" item-style="width: 80%">
-      <n-form ref="formRef" @submit.prevent="submit" :model="values" :rules="rules">
-        <n-form-item label="Motivational letter" path="motivationalLetter">
-            <n-input v-model:value="values.motivationalLetter" type="textarea" placeholder="I want to..." />
-        </n-form-item>
-        <n-form-item label="Certificate" path="certificate">
-            <ples-file-upload @update="update" @error="error" accept="application/pdf" />
-        </n-form-item>
-        
-        <n-form-item>
-            <n-button type="primary" attr-type="submit">Send application</n-button>
-        </n-form-item>
+  <n-space class="create-application" align="center" justify="center" item-style="width: 80%">
+    <n-form ref="formRef" @submit.prevent="submit" :model="values" :rules="rules">
+      <n-form-item label="Motivational letter" path="motivationalLetter">
+        <n-input v-model:value="values.motivationalLetter" type="textarea" placeholder="I want to..." />
+      </n-form-item>
+      <n-form-item label="Certificate" path="certificate">
+        <ples-file-upload @update="update" @error="error" accept="application/pdf" />
+      </n-form-item>
+
+      <n-form-item>
+        <n-button type="primary" attr-type="submit">Send application</n-button>
+      </n-form-item>
     </n-form>
-</n-space>
+  </n-space>
 </template>
 
 <script setup>
-  // eslint-disable-next-line sort-imports
-  import { trainerApplicationApi, clubApi, authApi } from '@/api';
-  import { onMounted, ref } from 'vue';
-  //import { computed } from '@vue/reactivity';
-  import { useMessage } from 'naive-ui';
-  import { useRoute } from 'vue-router';    
-  import { validationRules } from '@/utils';
-  import PlesFileUpload from '@/components/common/PlesFileUpload.vue';
+// eslint-disable-next-line sort-imports
+import PlesFileUpload from '@/components/common/PlesFileUpload.vue';
+import { ref } from 'vue'; // onMounted,
+import { trainerApplicationApi } from '@/api'; //, clubApi, authApi
+// import { computed } from '@vue/reactivity';
+import { useMessage } from 'naive-ui';
+import { useRoute } from 'vue-router';
+import { validationRules } from '@/utils';
 
-  const emit = defineEmits(['sent']);
-  
-  
-  const props = defineProps({
-      initialValues: {
-          type: Object,
-          default: () => ({
-              motivationalLetter: '',
-              certificate: '',
-            }),
-        },
-    });
+// const emit = defineEmits(['sent']);
 
-  /*  
+const props = defineProps({
+  initialValues: {
+    type: Object,
+    default: () => ({
+      motivationalLetter: '',
+      certificate: '',
+    }),
+  },
+});
+
+/*  
   onMounted(async () => {
     clubs.value = await clubApi.fetchAll();
     console.log(clubs.value);
     clubs.value = clubs.value.map(v => ({
         label: v.name,
         value: v.name,
-    }));
+      }));
   });
-*/    
-    const { required } = validationRules;
-    const rules = {
-        motivationalLetter: required,
-        certificate: required,
-    };
-    const message = useMessage();
-    const route = useRoute();
-    const values = ref(props.initialValues);
-    const formRef = ref(null);
-    console.log(route.params.id);
+*/
+const { required } = validationRules;
+const rules = {
+  motivationalLetter: required,
+  certificate: required,
+};
+const message = useMessage();
+const route = useRoute();
+const values = ref(props.initialValues);
+const formRef = ref(null);
+console.log(route.params.id);
 
-    const submit = async () => {
-        formRef.value?.validate(async errors => {
-            if (!errors) {
-                try {
-                await trainerApplicationApi.send({ ...values.value, id: route.params.id, clubId: route.params.clubId });  
-                    message.success('Success');
-                    emit('Sent');
-                } catch (err) {
-                    message.error(err.message);
-                }
-            }
-        });
-    };
-  
-    
+const submit = async () => {
+  formRef.value?.validate(async errors => {
+    if (!errors) {
+      try {
+        await trainerApplicationApi.send({ ...values.value, id: route.params.id, clubId: route.params.clubId });
+        message.success('Sent');
+      } catch (err) {
+        message.error(err.message);
+      }
+    }
+  });
+};
+
 const update = certificate => {
   values.value.certificate = certificate;
 };
@@ -82,8 +79,8 @@ const error = error => {
   message.error(error);
 };
 
-  // eslint-disable-next-line no-unused-vars
-  /* const error = error => {
+// eslint-disable-next-line no-unused-vars
+/* const error = error => {
     message.error(error);
   }; */
-  </script>
+</script>
