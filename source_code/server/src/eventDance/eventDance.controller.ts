11b166b18575a4fcdbb54a01e5b49982b4b0feb6
LDomic05 <lucijadomicsplit@gmail.com>
2023-01-08 23:27:56 +0100

    delete comments

--- source_code/server/src/eventDance/eventDance.controller.ts
+++ source_code/server/src/eventDance/eventDance.controller.ts
@@ -2,22 +2,9 @@ import { EventDance } from 'shared/database';
 // eslint-disable-next-line sort-imports
 import { Request, Response } from 'express';
 
-const test = async (req: Request, res: Response) => {
-  const testt = await EventDance.findAll();
-  return res.send(testt);
-};
-
 const eventDance = async (req: Request, res: Response) => {
   const eventDances = await EventDance.scope(['includeEventDance']).findAll();
   return res.send(eventDances);
 };
 
-/*
-const eventDance = async (req: Request, res: Response) => {
-  const eventDances = await EventDance.findAll({
-    include: 'includeEventDance',
-  });
-  return res.send(eventDances);
-}; */
-
-export { eventDance, test };
+export { eventDance };
