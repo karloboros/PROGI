80119b254ae1922e5ad0b7e906cc40bfb33efea8
ninaduric7 <ninaduric83@gmail.com>
2023-01-09 20:02:42 +0100

    index.js

--- source_code/client/src/api/index.js
+++ source_code/client/src/api/index.js
@@ -1,6 +1,7 @@
 export { default as authApi } from './auth';
 export { default as clubApi } from './club';
-export { default as trainerApplicationApi } from './trainerapplication';
+export { default as trainerApplicationApi } from './trainerApplication';
 export { default as danceApi } from './dance';
 export { default as eventApi } from './event';
+export { default as userCourseApi } from './userCourse';
 
