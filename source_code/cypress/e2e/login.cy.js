2a5ac0c63720149885705b527dba765d835c8116
Lnola <nola.luka@gmail.com>
2023-01-10 01:04:40 +0100

    Add beforeEach and afterEach for repeating parts of the test

--- source_code/cypress/e2e/login.cy.js
+++ source_code/cypress/e2e/login.cy.js
@@ -4,27 +4,27 @@ const clubOwnerUsername = 'owner';
 const adminUsername = 'admin';
 
 describe('Login Test', () => {
-  it('User should be able to log in', () => {
+  beforeEach(() => {
     cy.visit('/');
-    cy.login(userUsername);
+  });
+
+  afterEach(() => {
     cy.url().should('be.equal', Cypress.config('baseUrl'));
+  });
+
+  it('User should be able to log in', () => {
+    cy.login(userUsername);
   });
 
   it('Trainer should be able to log in', () => {
-    cy.visit('/');
     cy.login(trainerUsername);
-    cy.url().should('be.equal', Cypress.config('baseUrl'));
   });
 
   it('Club Owner should be able to log in', () => {
-    cy.visit('/');
     cy.login(clubOwnerUsername);
-    cy.url().should('be.equal', Cypress.config('baseUrl'));
   });
 
   it('Admin should be able to log in', () => {
-    cy.visit('/');
     cy.login(adminUsername);
-    cy.url().should('be.equal', Cypress.config('baseUrl'));
   });
 });
