7c10df418bd13e04d7fc787f8b1c14b21042e3e4
Lnola <nola.luka@gmail.com>
2023-01-10 01:02:31 +0100

    Add custom login command

--- source_code/cypress/support/commands.js
+++ source_code/cypress/support/commands.js
@@ -1,3 +1,9 @@
 Cypress.Commands.add('findByAriaLabel', ariaLabel => {
   cy.get(`[aria-label="${ariaLabel}"]`);
 });
+
+Cypress.Commands.add('login', role => {
+  cy.findByAriaLabel('username').type(role);
+  cy.findByAriaLabel('password').type('123');
+  cy.contains('Login').click();
+});
