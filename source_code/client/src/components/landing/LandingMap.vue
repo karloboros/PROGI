faddc296d804069eb5b500ae941a58629347df8f
Lnola <nola.luka@gmail.com>
2023-01-11 22:52:16 +0100

    Separate landing map view

---
+++ source_code/client/src/components/landing/LandingMap.vue
@@ -0,0 +1,15 @@
+<template>
+  <n-spin v-if="!items.length" class="spinner">
+    <ples-map :items="items" :filters="filters" height="78vh" />
+  </n-spin>
+  <ples-map v-else :items="items" :filters="filters" height="78vh" />
+</template>
+
+<script setup>
+import PlesMap from '@/components/common/PlesMap.vue';
+
+defineProps({
+  items: { type: Array, required: true },
+  filters: { type: Array, required: true },
+});
+</script>
