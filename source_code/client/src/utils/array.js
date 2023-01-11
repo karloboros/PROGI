af84a7bcf4e20f015937e8ced2156d6d438af440
Lnola <nola.luka@gmail.com>
2023-01-11 04:37:44 +0100

    Create array manipulation utils

---
+++ source_code/client/src/utils/array.js
@@ -0,0 +1,3 @@
+export const arrayDifference = (arr1, arr2) => {
+  return arr1.filter(x => !arr2.includes(x)).concat(arr2.filter(x => !arr1.includes(x)));
+};
--- source_code/client/src/utils/index.js
+++ source_code/client/src/utils/index.js
@@ -1,3 +1,4 @@
+export * from './array';
 export * from './dates';
 export { default as emailSuggestions } from './emailSuggestions';
 export { default as validationRules } from './rules';
