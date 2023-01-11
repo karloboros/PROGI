9a2b0d7999071221fe4fe6545f6a249dbdac2ef8
Lnola <nola.luka@gmail.com>
2023-01-11 04:54:53 +0100

    List events on landing map

--- source_code/client/src/components/landing/LandingPage.vue
+++ source_code/client/src/components/landing/LandingPage.vue
@@ -7,12 +7,25 @@
 
 <script setup>
 import { onMounted, ref } from 'vue';
-import { layers as lalalala } from '@/constants/map';
+import { eventApi } from '@/api';
 import PlesMap from '@/components/common/PlesMap.vue';
 
 const layers = ref([]);
 
-onMounted(() => {
-  layers.value = lalalala;
+onMounted(async () => {
+  const events = await eventApi.fetchAll();
+  const eventsLocations = events.map(({ id, name: content, location }) => {
+    const coordinates = location.coordinates.split(',');
+    return { id, content, coordinates };
+  });
+
+  layers.value = [
+    {
+      id: 0,
+      name: 'Events',
+      active: false,
+      locations: eventsLocations,
+    },
+  ];
 });
 </script>
