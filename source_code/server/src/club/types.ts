316075d3e12bfe61fb8d739d005a2e3318d0bd80
Lnola <nola.luka@gmail.com>
2023-01-11 22:53:17 +0100

    Add courses, events and location to club model types

--- source_code/server/src/club/types.ts
+++ source_code/server/src/club/types.ts
@@ -1,3 +1,7 @@
+import { ICourse } from 'course/types';
+import { IEvent } from 'event/types';
+import { ILocation } from 'location/types';
+
 enum ApprovalStatus {
   Pending,
   Approved,
@@ -13,6 +17,9 @@ interface IClub {
   description?: string;
   ownerId: number;
   locationId: number;
+  courses?: ICourse[];
+  events?: IEvent[];
+  location?: ILocation;
 }
 
 export { ApprovalStatus, IClub };
