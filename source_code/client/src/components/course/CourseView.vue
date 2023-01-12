21b8f8c97cb26120883405501d316b98d2a6331a
Lnola <nola.luka@gmail.com>
2023-01-12 19:57:34 +0100

    Use PlesCalendar to display lessons

--- source_code/client/src/components/course/CourseView.vue
+++ source_code/client/src/components/course/CourseView.vue
@@ -1,6 +1,9 @@
 <template>
   <ples-view :title="title" :data="course" class="py-3">
-    <n-image width="100" :src="trainerImage" />
+    <n-space vertical>
+      <n-image width="100" :src="trainerImage" />
+      <ples-calendar :lessons="lessons" class="py-7" />
+    </n-space>
   </ples-view>
 </template>
 
@@ -8,6 +11,7 @@
 import { onMounted, ref } from 'vue';
 import { courseApi } from '@/api';
 import { Gender } from '@/constants';
+import PlesCalendar from '@/components/common/PlesCalendar.vue';
 import PlesView from '@/components/common/PlesView.vue';
 
 const props = defineProps({
@@ -17,6 +21,7 @@ const props = defineProps({
 const title = ref('');
 const course = ref(null);
 const trainerImage = ref(null);
+const lessons = ref(null);
 
 onMounted(async () => {
   const {
@@ -32,9 +37,11 @@ onMounted(async () => {
     dance,
     location,
     trainer,
+    lessons: lessonsList,
   } = await courseApi.fetchById(props.routeId);
   title.value = name;
   trainerImage.value = trainer.image;
+  lessons.value = lessonsList.map(lesson => ({ ...lesson, location: location.name }));
   course.value = [
     { label: 'Description', value: description },
     { label: 'Capacity', value: capacity },
