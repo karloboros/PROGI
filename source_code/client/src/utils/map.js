e79feb904539dabfe475c412e4f4c2caebb002b9
Lnola <nola.luka@gmail.com>
2023-01-12 04:26:20 +0100

    Add getTimespanIds helper function

--- source_code/client/src/utils/map.js
+++ source_code/client/src/utils/map.js
@@ -1,3 +1,5 @@
+import { timespans } from '@/constants';
+
 const createFilterOptions = array => {
   const options = [{ value: 0, label: 'All' }];
   options.push(...array.map(({ id: value, name: label }) => ({ value, label })));
@@ -12,4 +14,12 @@ const getIds = array => {
   return array.map(({ id }) => id);
 };
 
-export { createFilterOptions, formatCoordinates, getIds };
+const getTimespanIds = lessons => {
+  return lessons.map(({ startTime }) => {
+    const startHour = new Date(startTime).getHours();
+    const { id } = timespans.find(timespan => startHour >= timespan.startHour && startHour < timespan.endHour);
+    return id;
+  });
+};
+
+export { createFilterOptions, formatCoordinates, getIds, getTimespanIds };
