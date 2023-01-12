99f12a07b1f468ba313e8211187c57ef196da9ce
Lnola <nola.luka@gmail.com>
2023-01-13 00:13:34 +0100

    Add courses to navigation

--- source_code/client/src/components/common/PlesNav.vue
+++ source_code/client/src/components/common/PlesNav.vue
@@ -15,6 +15,7 @@
                 <n-button @click="router.push({ name: 'Home' })" type="warning" text>Home</n-button>
                 <n-button @click="router.push({ name: 'Landing' })" type="warning" text>Landing</n-button>
                 <n-button @click="router.push({ name: 'Profile' })" type="warning" text>Profile</n-button>
+                <n-button @click="router.push({ name: 'CourseList' })" type="warning" text>Courses</n-button>
                 <n-button @click="logout" type="warning" text>Logout</n-button>
               </slot>
             </slot>
