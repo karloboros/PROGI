3c8d2c263d07d2d533bd0f671d7d7f048a0f5b12
Ana Vrabec <ana.vrabec@estudent.hr>
2023-01-12 22:23:11 +0100

    Modify base url

--- source_code/cypress.config.js
+++ source_code/cypress.config.js
@@ -3,7 +3,7 @@ import seed from './cypress/tasks/seed.js';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3000/',
     setupNodeEvents,
   },
 });
