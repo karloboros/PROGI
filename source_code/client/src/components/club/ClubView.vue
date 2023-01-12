a6d5c093399a0a36152301a78a75cecaeee6be6a
Lnola <nola.luka@gmail.com>
2023-01-12 23:03:11 +0100

    Add map to club view

--- source_code/client/src/components/club/ClubView.vue
+++ source_code/client/src/components/club/ClubView.vue
@@ -1,10 +1,13 @@
 <template>
-  <ples-view :title="title" :data="club" />
+  <ples-view :title="title" :data="club" class="py-3">
+    <club-map :location="clubLocation" />
+  </ples-view>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
 import { clubApi } from '@/api';
+import ClubMap from './ClubMap.vue';
 import PlesView from '@/components/common/PlesView.vue';
 
 const props = defineProps({
@@ -13,10 +16,12 @@ const props = defineProps({
 
 const title = ref('');
 const club = ref(null);
+const clubLocation = ref(null);
 
 onMounted(async () => {
   const { name, phone, email, description, owner, location, dances } = await clubApi.fetchByIdWithDances(props.routeId);
   title.value = name;
+  clubLocation.value = location;
   club.value = [
     { label: 'Phone', value: phone },
     { label: 'Email', value: email },
