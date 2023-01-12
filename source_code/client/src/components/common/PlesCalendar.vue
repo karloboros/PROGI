8e4ca077359f2a378450a6bd7fffd9d0837ff73e
Lnola <nola.luka@gmail.com>
2023-01-12 19:57:13 +0100

    Turn PlesCalendar into a generic component

--- source_code/client/src/components/common/PlesCalendar.vue
+++ source_code/client/src/components/common/PlesCalendar.vue
@@ -1,53 +1,45 @@
 <template>
-  <n-space align="center" justify="center" item-style="width: 80%">
-    <Qalendar :events="events" />
+  <n-space align="center" justify="center" item-style="width: 100%">
+    <Qalendar :events="events" :config="config" />
   </n-space>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
-import { format } from 'date-fns';
-import { lessonApi } from '@/api';
+import { ref, watchEffect } from 'vue';
+import { formatDate } from '@/utils';
 import { Qalendar } from 'qalendar';
-import { useRoute } from 'vue-router';
 
-const route = useRoute();
+const config = {
+  defaultMode: 'month',
+};
+
+const props = defineProps({
+  lessons: { type: Array, default: () => [] },
+});
+
 const events = ref([]);
-const courseId = route.params.courseId;
 
-onMounted(async () => {
-  const data = await lessonApi.fetchAll(courseId);
-  for (const lesson of data) {
-    let date = new Date(lesson.startTime);
-    const startTime = format(date, 'yyyy-dd-MM hh:mm');
-    date = new Date(lesson.endTime);
-    const endTime = format(date, 'yyyy-dd-MM hh:mm');
-    lesson.value = {
-      title: lesson.course.name,
-      time: {
-        start: startTime,
-        end: endTime,
-      },
-      topic: 'Capacity: ' + lesson.course.capacity,
-      description: lesson.course.description,
-      id: lesson.id,
-      color: 'blue',
-      disableDnD: ['month', 'week', 'day'],
-      disableResize: ['week', 'day'],
-    };
-    events.value.push(lesson.value);
-  }
+watchEffect(() => {
+  const data = props.lessons.map(({ id, startTime, endTime, location }) => {
+    const title = 'Lesson';
+    const start = formatDate(startTime);
+    const end = formatDate(endTime);
+    const time = { start, end };
+    const color = 'yellow';
+    return { id, title, location, time, color };
+  });
+  events.value = data;
 });
 </script>
 
 <style>
 .event-flyout__info-wrapper {
-  background-color: black;
+  background-color: var(--n-color) !important;
 }
 .calendar-header__mode-options {
-  background-color: black !important;
+  background-color: var(--n-color) !important;
 }
 .date-picker__week-picker {
-  background-color: black !important;
+  background-color: var(--n-color) !important;
 }
 </style>
