6003037129aec14f1ad5c0a8806e07ff123285d0
Lnola <nola.luka@gmail.com>
2023-01-12 19:10:16 +0100

    Create simple course view

---
+++ source_code/client/src/components/course/CourseView.vue
@@ -0,0 +1,51 @@
+<template>
+  <ples-view :title="title" :data="course" class="py-3">
+    <n-image width="100" :src="trainerImage" />
+  </ples-view>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import { courseApi } from '@/api';
+import { Gender } from '@/constants';
+import PlesView from '@/components/common/PlesView.vue';
+
+const props = defineProps({
+  routeId: { type: Number, required: true },
+});
+
+const title = ref('');
+const course = ref(null);
+const trainerImage = ref(null);
+
+onMounted(async () => {
+  const {
+    name,
+    description,
+    capacity,
+    minAge,
+    maxAge,
+    gender,
+    applicationDeadline,
+    additionalRules,
+    club,
+    dance,
+    location,
+    trainer,
+  } = await courseApi.fetchById(props.routeId);
+  title.value = name;
+  trainerImage.value = trainer.image;
+  course.value = [
+    { label: 'Description', value: description },
+    { label: 'Capacity', value: capacity },
+    { label: 'Age group', value: minAge && maxAge ? `${minAge} - ${maxAge}` : null },
+    { label: 'Gender', value: Object.keys(Gender)[gender] },
+    { label: 'Application deadline', value: new Date(applicationDeadline) },
+    { label: 'Additional rules', value: additionalRules },
+    { label: 'Club', value: club.name },
+    { label: 'Dance', value: dance.name },
+    { label: 'Location', value: location.name },
+    { label: 'Trainer', value: trainer.fullName },
+  ];
+});
+</script>
