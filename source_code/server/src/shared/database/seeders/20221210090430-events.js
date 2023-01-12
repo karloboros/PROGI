aaa3a677c1a457566fd3382d549aad06bfd227bc
Ana Vrabec <ana.vrabec@estudent.hr>
2023-01-12 01:36:06 +0100

    Modify seed

--- source_code/server/src/shared/database/seeders/20221210090430-events.js
+++ source_code/server/src/shared/database/seeders/20221210090430-events.js
@@ -1,64 +1,28 @@
 const TABLE_NAME = 'events';
 
+const createEvent = (name, clubId, locationId, startTime) => {
+  return {
+    name,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+    image: '/images/dances/thumbnail.png',
+    clubId,
+    locationId,
+    startTime,
+  };
+};
+
 module.exports = {
   up(queryInterface) {
     const events = [];
-    events.push({
-      name: `Event #1`,
-      description: `bla bla`,
-      image: `image one`,
-      startTime: new Date('2023'),
-      clubId: 1,
-      locationId: 1,
-    });
-    events.push({
-      name: `Event #2`,
-      description: `bla bla`,
-      image: `image one`,
-      startTime: new Date('2023'),
-      clubId: 2,
-      locationId: 2,
-    });
-    events.push({
-      name: `Event #3`,
-      description: `bla bla`,
-      image: `image one`,
-      startTime: new Date('2023'),
-      clubId: 2,
-      locationId: 3,
-    });
-    events.push({
-      name: `Event #4`,
-      description: `bla bla`,
-      image: `image one`,
-      startTime: new Date('2023'),
-      clubId: 3,
-      locationId: 4,
-    });
-    events.push({
-      name: `Event #5`,
-      description: `bla bla`,
-      image: `image one`,
-      startTime: new Date('2023'),
-      clubId: 4,
-      locationId: 5,
-    });
-    events.push({
-      name: `Ples studenata`,
-      description: `tango`,
-      image: `image one`,
-      startTime: new Date('2023'),
-      clubId: 5,
-      locationId: 4,
-    });
-    events.push({
-      name: `Ples u domu`,
-      description: `valcer`,
-      image: `image two`,
-      startTime: new Date('2023'),
-      clubId: 5,
-      locationId: 5,
-    });
+    events.push(createEvent('Event Fun', 1, 1, new Date('2023')));
+    events.push(createEvent('Dance party', 2, 2, new Date('2023-01-25')));
+    events.push(createEvent('Tango night', 2, 3, new Date('2023-04-20')));
+    events.push(createEvent('Valcer', 3, 4, new Date('2023-03-02')));
+    events.push(createEvent('Dance, dance, dance', 4, 5, new Date('2023-01-15')));
+    events.push(createEvent('Formal dance ball', 5, 5, new Date('2023-03-25')));
+    events.push(createEvent('Dance festival Zagreb', 5, 6, new Date('2023-01-11')));
+    events.push(createEvent('Just dance', 5, 7, new Date('2023-01-20')));
+    events.push(createEvent('Party', 5, 8, new Date('2023-02-25')));
     return queryInterface.bulkInsert(TABLE_NAME, events);
   },
 
