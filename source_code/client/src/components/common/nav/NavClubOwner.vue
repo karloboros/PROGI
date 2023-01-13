3feff7db45855e31a48a64be9e30384af59c3bf2
Lnola <nola.luka@gmail.com>
2023-01-13 15:57:19 +0100

    Add owners club fetch

--- source_code/client/src/components/common/nav/NavClubOwner.vue
+++ source_code/client/src/components/common/nav/NavClubOwner.vue
@@ -1,11 +1,23 @@
 <template>
   <n-space>
     <n-button @click="router.push({ name: 'Landing' })" type="warning" text>Landing</n-button>
+    <n-button @click="router.push({ name: 'CourseList', params: { id: clubId } })" type="warning" text>
+      Courses
+    </n-button>
     <n-button @click="router.push({ name: 'Profile' })" type="warning" text>Profile</n-button>
   </n-space>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
+import { clubApi } from '@/api';
 import { useRouter } from 'vue-router';
+
+const clubId = ref(null);
 const router = useRouter();
+
+onMounted(async () => {
+  const club = await clubApi.fetchByOwner();
+  clubId.value = club.id;
+});
 </script>
