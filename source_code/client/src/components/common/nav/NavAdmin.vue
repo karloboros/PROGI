a405109d32c8a6ca2f6669f8c9ff447c8552369e
Ana Vrabec <ana.vrabec@estudent.hr>
2023-01-13 18:00:56 +0100

    Update admin navigation

--- source_code/client/src/components/common/nav/NavAdmin.vue
+++ source_code/client/src/components/common/nav/NavAdmin.vue
@@ -2,6 +2,10 @@
   <n-space>
     <n-button @click="router.push({ name: 'Landing' })" type="warning" text>Landing</n-button>
     <n-button @click="router.push({ name: 'Profile' })" type="warning" text>Profile</n-button>
+    <n-button @click="router.push({ name: 'UsersList' })" type="warning" text>Users</n-button>
+    <n-button @click="router.push({ name: 'ClubsList' })" type="warning" text>Clubs</n-button>
+    <n-button @click="router.push({ name: 'ClubApproval' })" type="warning" text>Pending clubs</n-button>
+    <n-button @click="router.push({ name: 'DancesList' })" type="warning" text>Dances</n-button>
   </n-space>
 </template>
 
