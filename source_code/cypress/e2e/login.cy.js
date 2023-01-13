208b0f00d9d84441398269babb1e723a5c419ee5
Lnola <nola.luka@gmail.com>
2023-01-13 22:43:50 +0100

    Fix login test

--- source_code/cypress/e2e/login.cy.js
+++ source_code/cypress/e2e/login.cy.js
@@ -10,26 +10,27 @@ describe('Login Test', () => {
 
   beforeEach(() => {
     cy.visit('/');
-  });
-
-  afterEach(() => {
-    cy.url().should('be.equal', Cypress.config('baseUrl'));
+    cy.contains('Login').click();
   });
 
   it('User should be able to log in', () => {
     cy.login(userUsername);
+    cy.url().should('be.equal', Cypress.config('baseUrl'));
   });
 
   it('Trainer should be able to log in', () => {
     cy.login(trainerUsername);
+    cy.url().should('be.equal', Cypress.config('baseUrl'));
   });
 
   it('Club Owner should be able to log in', () => {
     cy.login(clubOwnerUsername);
+    cy.url().should('be.equal', Cypress.config('baseUrl'));
   });
 
   it('Admin should be able to log in', () => {
     cy.login(adminUsername);
+    cy.url().should('be.equal', Cypress.config('baseUrl'));
   });
 
   it('User with username field missing should not be able to log in', () => {
@@ -37,16 +38,16 @@ describe('Login Test', () => {
     cy.contains('Login').click();
     const caught = {
       message: null,
-    }
+    };
 
     Cypress.on('uncaught:exception', (err, runnable, promise) => {
       caught.message = err.message;
       if (promise) return false;
     });
 
-    cy.wrap(caught).should((c) => {
+    cy.wrap(caught).should(c => {
       expect(c.message).to.include('unhandled promise rejection');
-    })
+    });
 
     cy.url().should('be.equal', 'http://localhost:3000/auth');
   });
